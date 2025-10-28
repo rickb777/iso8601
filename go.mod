588ffdb12c2c6139ab75f5c1af7fc7d0f50685f7
Rick <rick.beton@gmail.com>
2025-10-28 10:12:19 +0000

    updated dependencies; now using mage

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/rickb777/iso8601/v2
 go 1.24.1
 
 require (
+	github.com/magefile/mage v1.15.0
+	github.com/rickb777/expect v1.0.0
+)
+
+require (
 	github.com/google/go-cmp v0.7.0 // indirect
-	github.com/rickb777/expect v0.24.0 // indirect
 	github.com/rickb777/plural v1.4.4 // indirect
 )
+
+tool github.com/magefile/mage
---
+++ magefiles/build.go
@@ -0,0 +1,25 @@
+// See https://magefile.org/
+
+//go:build mage
+
+// Build steps for the boltqueue API:
+package main
+
+import (
+	"github.com/magefile/mage/sh"
+)
+
+var Default = Build
+
+func Build() error {
+	if err := sh.RunV("go", "test", "./..."); err != nil {
+		return err
+	}
+	if err := sh.RunV("gofmt", "-l", "-w", "-s", "."); err != nil {
+		return err
+	}
+	if err := sh.RunV("go", "vet", "./..."); err != nil {
+		return err
+	}
+	return nil
+}
