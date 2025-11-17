f49f5692cd71940aaaab0ed6b70f1bf14d246e23
Rick <1358735+rickb777@users.noreply.github.com>
2025-11-17 14:40:03 +0000

    simplified tests

--- iso8601_test.go
+++ iso8601_test.go
@@ -1,6 +1,7 @@
 package iso8601
 
 import (
+	"strings"
 	"testing"
 	"time"
 
@@ -19,306 +20,202 @@ type TestCase struct {
 	Second      int
 	MilliSecond int
 
-	Zone            float64
-	ShouldFailParse bool
-
-	ShouldInvalidRange      bool
-	RangeElementWhenInvalid string
+	Zone float64
 }
 
-func (tc TestCase) CheckError(err error, t *testing.T) bool {
-	if err != nil {
-		if tc.ShouldInvalidRange {
-			re, ok := err.(*RangeError)
-			if !ok {
-				t.Fatalf("Found error %s of type %T but was expecting a RangeError", err, err)
-			}
+func TestParse_ok(t *testing.T) {
+	var goodCases = []TestCase{
+		{
+			Using: "2017-04-24T09:41:34.502+0100",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			MilliSecond: 502,
+			Zone:        1,
+		},
+		{
+			Using: "2017-04-24T09:41+0100",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41,
+			Zone: 1,
+		},
+		{
+			Using: "2017-04-24T09+0100",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9,
+			Zone: 1,
+		},
+		{
+			Using: "2017-04-24T",
+			Year:  2017, Month: 4, Day: 24,
+		},
+		{
+			Using: "2017-04-24",
+			Year:  2017, Month: 4, Day: 24,
+		},
+		{
+			Using: "2017-04-24T09:41:34+0100",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			Zone: 1,
+		},
+		{
+			Using: "2017-04-24T09:41:34.502-0100",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			MilliSecond: 502,
+			Zone:        -1,
+		},
+		{
+			Using: "2017-04-24T09:41:34.502-01:00",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			MilliSecond: 502,
+			Zone:        -1,
+		},
+		{
+			Using: "2017-04-24T09:41-01:00",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41,
+			Zone: -1,
+		},
+		{
+			Using: "2017-04-24T09-01:00",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9,
+			Zone: -1,
+		},
+		{
+			Using: "2017-04-24T09:41:34-0100",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			Zone: -1,
+		},
+		{
+			Using: "2017-04-24T09:41:34.502Z",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			MilliSecond: 502,
+			Zone:        0,
+		},
+		{
+			Using: "2017-04-24T09:41:34Z",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			Zone: 0,
+		},
+		{
+			Using: "2017-04-24T09:41Z",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41,
+			Zone: 0,
+		},
+		{
+			Using: "2017-04-24T09Z",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9,
+			Zone: 0,
+		},
+		{
+			Using: "2017-04-24T09:41:34.089",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			MilliSecond: 89,
+			Zone:        0,
+		},
+		{
+			Using: "2017-04-24T09:41",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41,
+			Zone: 0,
+		},
+		{
+			Using: "2017-04-24T09",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9,
+			Zone: 0,
+		},
+		{
+			Using: "2017-04-24T09:41:34.009",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			MilliSecond: 9,
+			Zone:        0,
+		},
+		{
+			Using: "2017-04-24T09:41:34.893",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			MilliSecond: 893,
+			Zone:        0,
+		},
+		{
+			Using: "2017-04-24T09:41:34.89312523Z",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			MilliSecond: 893,
+			Zone:        0,
+		},
+		{
+			Using: "2017-04-24T09:41:34.502-0530",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			MilliSecond: 502,
+			Zone:        -5.5,
+		},
+		{
+			Using: "2017-04-24T09:41:34.502+0530",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			MilliSecond: 502,
+			Zone:        5.5,
+		},
+		{
+			Using: "2017-04-24T09:41:34.502+05:30",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			MilliSecond: 502,
+			Zone:        5.5,
+		},
 
-			if tc.RangeElementWhenInvalid != "" && re.Element != tc.RangeElementWhenInvalid {
-				t.Fatalf("Expected a range error on %q but encountered %q: %s", tc.RangeElementWhenInvalid, re.Element, err)
-			}
-
-			return true
-		}
-		if tc.ShouldFailParse {
-			return true
-		}
-
-		t.Fatal(err)
-		return false
+		{
+			Using: "2017-04-24T09:41:34.502+05:45",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			MilliSecond: 502,
+			Zone:        5.75,
+		},
+		{
+			Using: "2017-04-24T09:41:34.502+00",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			MilliSecond: 502,
+			Zone:        0,
+		},
+		{
+			Using: "2017-04-24T09:41:34.502+0000",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			MilliSecond: 502,
+			Zone:        0,
+		},
+		{
+			Using: "2017-04-24T09:41:34.502+00:00",
+			Year:  2017, Month: 4, Day: 24,
+			Hour: 9, Minute: 41, Second: 34,
+			MilliSecond: 502,
+			Zone:        0,
+		},
 	}
 
-	if err == nil && (tc.ShouldFailParse || tc.ShouldInvalidRange) {
-		reason := "fail to parse"
-		if tc.ShouldInvalidRange {
-			reason = "catch an invalid date range"
-		}
-		t.Fatalf("Expected test case %s", reason)
-		return true
-	}
-
-	return false
-}
-
-var cases = []TestCase{
-	{
-		Using: "2017-04-24T09:41:34.502+0100",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		MilliSecond: 502,
-		Zone:        1,
-	},
-	{
-		Using: "2017-04-24T09:41+0100",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41,
-		Zone: 1,
-	},
-	{
-		Using: "2017-04-24T09+0100",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9,
-		Zone: 1,
-	},
-	{
-		Using: "2017-04-24T",
-		Year:  2017, Month: 4, Day: 24,
-	},
-	{
-		Using: "2017-04-24",
-		Year:  2017, Month: 4, Day: 24,
-	},
-	{
-		Using: "2017-04-24T09:41:34+0100",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		Zone: 1,
-	},
-	{
-		Using: "2017-04-24T09:41:34.502-0100",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		MilliSecond: 502,
-		Zone:        -1,
-	},
-	{
-		Using: "2017-04-24T09:41:34.502-01:00",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		MilliSecond: 502,
-		Zone:        -1,
-	},
-	{
-		Using: "2017-04-24T09:41-01:00",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41,
-		Zone: -1,
-	},
-	{
-		Using: "2017-04-24T09-01:00",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9,
-		Zone: -1,
-	},
-	{
-		Using: "2017-04-24T09:41:34-0100",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		Zone: -1,
-	},
-	{
-		Using: "2017-04-24T09:41:34.502Z",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		MilliSecond: 502,
-		Zone:        0,
-	},
-	{
-		Using: "2017-04-24T09:41:34Z",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		Zone: 0,
-	},
-	{
-		Using: "2017-04-24T09:41Z",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41,
-		Zone: 0,
-	},
-	{
-		Using: "2017-04-24T09Z",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9,
-		Zone: 0,
-	},
-	{
-		Using: "2017-04-24T09:41:34.089",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		MilliSecond: 89,
-		Zone:        0,
-	},
-	{
-		Using: "2017-04-24T09:41",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41,
-		Zone: 0,
-	},
-	{
-		Using: "2017-04-24T09",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9,
-		Zone: 0,
-	},
-	{
-		Using: "2017-04-24T09:41:34.009",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		MilliSecond: 9,
-		Zone:        0,
-	},
-	{
-		Using: "2017-04-24T09:41:34.893",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		MilliSecond: 893,
-		Zone:        0,
-	},
-	{
-		Using: "2017-04-24T09:41:34.89312523Z",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		MilliSecond: 893,
-		Zone:        0,
-	},
-	{
-		Using: "2017-04-24T09:41:34.502-0530",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		MilliSecond: 502,
-		Zone:        -5.5,
-	},
-	{
-		Using: "2017-04-24T09:41:34.502+0530",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		MilliSecond: 502,
-		Zone:        5.5,
-	},
-	{
-		Using: "2017-04-24T09:41:34.502+05:30",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		MilliSecond: 502,
-		Zone:        5.5,
-	},
-
-	{
-		Using: "2017-04-24T09:41:34.502+05:45",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		MilliSecond: 502,
-		Zone:        5.75,
-	},
-	{
-		Using: "2017-04-24T09:41:34.502+00",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		MilliSecond: 502,
-		Zone:        0,
-	},
-	{
-		Using: "2017-04-24T09:41:34.502+0000",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		MilliSecond: 502,
-		Zone:        0,
-	},
-	{
-		Using: "2017-04-24T09:41:34.502+00:00",
-		Year:  2017, Month: 4, Day: 24,
-		Hour: 9, Minute: 41, Second: 34,
-		MilliSecond: 502,
-		Zone:        0,
-	},
-
-	// Invalid Parse Test Cases
-	{
-		Using:           "2017-04-24T09:41:34.502-00",
-		ShouldFailParse: true,
-	},
-	{
-		Using:           "2017-04-24T09:41:34.502-0000",
-		ShouldFailParse: true,
-	},
-	{
-		Using:           "2017-04-24T09:41:34.502-00:00",
-		ShouldFailParse: true,
-	},
-
-	// Invalid Range Test Cases
-	{
-		Using:                   "2017-00-01T00:00:00.000+00:00",
-		ShouldInvalidRange:      true,
-		RangeElementWhenInvalid: "month",
-	},
-	{
-		Using:                   "2017-13-01T00:00:00.000+00:00",
-		ShouldInvalidRange:      true,
-		RangeElementWhenInvalid: "month",
-	},
-
-	{
-		Using:                   "2017-01-00T00:00:00.000+00:00",
-		ShouldInvalidRange:      true,
-		RangeElementWhenInvalid: "day",
-	},
-	{
-		Using:                   "2017-01-32T00:00:00.000+00:00",
-		ShouldInvalidRange:      true,
-		RangeElementWhenInvalid: "day",
-	},
-	{
-		Using:                   "2019-02-29T00:00:00.000+00:00",
-		ShouldInvalidRange:      true,
-		RangeElementWhenInvalid: "day",
-	},
-	{
-		Using:                   "2020-02-30T00:00:00.000+00:00", // Leap year
-		ShouldInvalidRange:      true,
-		RangeElementWhenInvalid: "day",
-	},
-
-	{
-		Using:                   "2017-01-01T24:00:00.000+00:00",
-		ShouldInvalidRange:      true,
-		RangeElementWhenInvalid: "hour",
-	},
-
-	{
-		Using:                   "2017-01-01T00:60:00.000+00:00",
-		ShouldInvalidRange:      true,
-		RangeElementWhenInvalid: "minute",
-	},
-
-	{
-		Using:                   "2017-01-01T00:00:60.000+00:00",
-		ShouldInvalidRange:      true,
-		RangeElementWhenInvalid: "second",
-	},
-}
-
-func TestParse(t *testing.T) {
-	for _, c := range cases {
+	for _, c := range goodCases {
 		t.Run(c.Using, func(t *testing.T) {
 			d, err := ParseString(c.Using) // also encompasses testing of Parse([]byte).
-			if c.CheckError(err, t) {
-				return
+			if err != nil {
+				t.Fatal(err)
 			}
-			t.Log(d)
 
 			expect.Number(d.Year()).ToBe(t, c.Year)
-			expect.Number((d.Month())).ToBe(t, c.Month)
+			expect.Number(d.Month()).ToBe(t, c.Month)
 			expect.Number(d.Day()).ToBe(t, c.Day)
 			expect.Number(d.Hour()).ToBe(t, c.Hour)
 			expect.Number(d.Minute()).ToBe(t, c.Minute)
@@ -328,7 +225,84 @@ func TestParse(t *testing.T) {
 			_, z := d.Zone()
 			expect.Number(float64(z)/3600).ToBe(t, c.Zone)
 		})
+	}
+}
 
+type ErrorCase struct {
+	Using   string
+	Message string
+}
+
+func TestParse_error(t *testing.T) {
+	var errorCases = []ErrorCase{
+		// Invalid Parse Test Cases
+		{
+			Using:   "2017-04-24T09:41:34.502-00",
+			Message: `Cannot parse "-00": invalid zone`,
+		},
+		{
+			Using:   "2017-04-24T09:41:34.502-0000",
+			Message: `Cannot parse "-0000": invalid zone`,
+		},
+		{
+			Using:   "2017-04-24T09:41:34.502-00:00",
+			Message: `Cannot parse "-00:00": invalid zone`,
+		},
+
+		// Invalid Range Test Cases
+		{
+			Using:   "2017-00-01T00:00:00.000+00:00",
+			Message: `Cannot parse "2017-00-01T00:00:00.000+00:00": month 0 is not in range 1-12`,
+		},
+		{
+			Using:   "2017-13-01T00:00:00.000+00:00",
+			Message: `Cannot parse "2017-13-01T00:00:00.000+00:00": month 13 is not in range 1-12`,
+		},
+
+		{
+			Using:   "2017-01-00T00:00:00.000+00:00",
+			Message: `Cannot parse "2017-01-00T00:00:00.000+00:00": day 0 is not in range 1-31`,
+		},
+		{
+			Using:   "2017-01-32T00:00:00.000+00:00",
+			Message: `Cannot parse "2017-01-32T00:00:00.000+00:00": day 32 is not in range 1-31`,
+		},
+		{
+			Using:   "2019-02-29T00:00:00.000+00:00",
+			Message: `Cannot parse "2019-02-29T00:00:00.000+00:00": day 29 is not in range 1-28`,
+		},
+		{
+			Using:   "2020-02-30T00:00:00.000+00:00", // Leap year
+			Message: `Cannot parse "2020-02-30T00:00:00.000+00:00": day 30 is not in range 1-29`,
+		},
+
+		{
+			Using:   "2017-01-01T24:00:00.000+00:00",
+			Message: `Cannot parse "2017-01-01T24:00:00.000+00:00": hour 24 is not in range 0-23`,
+		},
+
+		{
+			Using:   "2017-01-01T00:60:00.000+00:00",
+			Message: `Cannot parse "2017-01-01T00:60:00.000+00:00": minute 60 is not in range 0-59`,
+		},
+
+		{
+			Using:   "2017-01-01T00:00:60.000+00:00",
+			Message: `Cannot parse "2017-01-01T00:00:60.000+00:00": second 60 is not in range 0-59`,
+		},
+	}
+
+	for _, c := range errorCases {
+		t.Run(c.Using, func(t *testing.T) {
+			_, err := ParseString(c.Using) // also encompasses testing of Parse([]byte).
+			if err == nil {
+				t.Fatalf("Expected error containing %q", c.Message)
+			} else {
+				if !strings.Contains(err.Error(), c.Message) {
+					t.Errorf("Expected error message %q to contain %q", err.Error(), c.Message)
+				}
+			}
+		})
 	}
 }
 
